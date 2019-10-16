cad68854bcf4e787135fbb653639023bf423b3dd
berseh <bersehe@gmail.com>
2019-10-16 12:41:13 -0400

    this is the bash scripting files

---
+++ firstscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Sample first script from Lolubyte IT Computer Training Center
+#Author:
+#Date:
+#Contact Details
+#Script Usage
+#Change history: (Who, when, why)
+echo "Hey there....." 
